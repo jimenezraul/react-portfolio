cdc3395b94e351035948ce3cc62f15c7c0f3d7f0
Raul <jimenezraul1981@gmail.com>
2022-05-02 08:40:48 -0400

    add footer component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React, { useState } from "react";
 import "./App.css";
+import Footer from "./component/Footer";
 import Navbar from "./component/Navbar";
 
 function App() {
   const [currentSelect, setCurrentSelect] = useState("About Me");
 
   return (
-    <div className="App">
+    <div className="App flex flex-col min-h-screen justify-between">
       <Navbar
         currentSelect={currentSelect}
         setCurrentSelect={setCurrentSelect}
       />
       <h1>{currentSelect}</h1>
+      <Footer />
     </div>
   );
 }
---
+++ src/component/Footer/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <footer className="footer bg-gray-200 p-3">
+        <div className="container mx-auto">
+            <div className="flex justify-center">
+                    <p>
+                        My Portfolio 
+                <strong> &copy; Raul Jimenez</strong>  
+                </p>
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
