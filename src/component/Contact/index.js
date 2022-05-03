9ff81f2ccdfc5bf5192fa4c5a5122c5c4e98f962
Raul <jimenezraul1981@gmail.com>
2022-05-03 00:04:18 -0400

    rename placeholder in input form

--- src/component/Contact/index.js
+++ src/component/Contact/index.js
@@ -59,7 +59,7 @@ const Contact = () => {
                     defaultValue={name}
                     onBlur={handleChange}
                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="John Doe"
+                    placeholder="Name"
                     required
                   />
                 </div>
@@ -76,7 +76,7 @@ const Contact = () => {
                     defaultValue={email}
                     onBlur={handleChange}
                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="john.doe@company.com"
+                    placeholder="Email address"
                     required
                   />
                 </div>
